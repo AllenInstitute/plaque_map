eacb74b7bde111d85fcd64d58dce4564cba31134
Jennifer Whitesell <jenniferwh@localadmins-MacBook-Pro-9.local>
2018-10-15 12:28:06 -0700

    Added path information for Brain Explorer on Mac

--- brain_explorer_plaque_colormap.py
+++ brain_explorer_plaque_colormap.py
@@ -11,8 +11,12 @@ is included as plaque_densities_per_structure.csv.
 Before using this code, download Brain Explorer from 
 http://mouse.brain-map.org/static/brainexplorer.
 Run Brain Explorer and download the Allen Mouse Common Coordinate Framework
-atlas using the prompts in the Brain Explorer program. This code modifies the
-ontology_v2.csv file installed by Brain Explorer to customize the colormap. 
+atlas using the prompts in the Brain Explorer program. The ontology_v2.csv file
+will be installed in C:\Users\*USERNAME*\AppData\Local\Allen Institute\Brain Explorer 2\Atlases\Allen Mouse Brain Common Coordinate Framework
+on a PC, or /Users/*USERNAME*/Library/Application Support/Brain Explorer 2/Atlases/Allen Mouse Brain Common Coordinate Framework
+on a mac. This code modifies the ontology_v2.csv file installed by Brain Explorer 
+to customize the colormap. See http://help.brain-map.org/display/mousebrain/Brain+Explorer 
+forcmore information about Brain Explorer installation.
 
 Parameters
 ----------
@@ -36,7 +40,7 @@ from matplotlib import cm
 mpl.rcParams['pdf.fonttype'] = 42
 
 # Find the path to an existing ontology document that was installed through the Brain Explorer interface
-path = r'C:\Users\jenniferwh\AppData\Local\Allen Institute\Brain Explorer 2\Atlases\Allen Mouse Brain Common Coordinate Framework'
+path = r'/Users/jenniferwh/Library/Application Support/Brain Explorer 2/Atlases/Allen Mouse Brain Common Coordinate Framework'
 dat = pd.read_csv(os.path.join(path, 'ontology_v2.csv'))
 
 unionize_dat = pd.read_csv('plaque_densities_per_structure.csv')
@@ -70,7 +74,7 @@ def get_cmap(group, age, colormap=cm.gray, scale=1):
     return rgb_vals, n
 
 def main():
-    rgb_vals, n = get_cmap(args.mouse_line, str(args.age)+' mo', cm.hot, args.scale)
+    rgb_vals, n = get_cmap(args.mouse_line, str(args.age)+' mo', cm.inferno, args.scale)
     for key in rgb_vals.keys():
         dat.loc[dat['database_id'] == key, 'red'] = int(rgb_vals[key][0])
         dat.loc[dat['database_id'] == key, 'green'] = int(rgb_vals[key][1])
