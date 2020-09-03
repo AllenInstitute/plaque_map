13742d4057fbc4c75fae219200b9ba13403b9436
Jennifer Whitesell <jennifer.whitesell@gmail.com>
2020-09-03 12:22:59 -0700

    clean up

--- make_tau_map_single_brain.py
+++ make_tau_map_single_brain.py
@@ -5,7 +5,7 @@ Created on Tue Jul 31 14:43:02 2018
 @author: JENNIFERWH
 Creates planar image sections from the 3D heatmap of plaque density per structure
 as described in https://www.biorxiv.org/content/early/2018/08/18/395236. 
-Data from the paper is included in the plaque_densities_per_structure.csv file.
+Ldata from the paper is included in the plaque_densities_per_structure.csv file.
 
 Parameters
 ----------
@@ -29,31 +29,46 @@ from matplotlib import cm
 import operator
 import json
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-path = r'/Users/jenniferwh/Dropbox/Diamond_collaboration'
-dat = pd.read_csv('/Users/jenniferwh/Dropbox (Allen Institute)/Diamond_collaboration/data_files/thresholded_data_LR.csv')
-dat = pd.melt(dat, id_vars = ['SampleID', 'Condition'], value_name = 'density', var_name='acronym')
 mcc = MouseConnectivityCache(manifest_file = '..connectivity/mouse_connectivity_manifest.json')
 st = mcc.get_structure_tree()
 ia_map = st.get_id_acronym_map()
-dat['acronym'] = [acronym[:-2] for acronym in dat['acronym'].values] #throwing away hemisphere and averaging for now
-for structure in dat['acronym'].unique():
-    dat.loc[dat['acronym'] == structure, 'structure_id'] = ia_map[structure]
 
-def get_mean_value_per_structure(group, structure_ids):
+path = r'/Users/jenniferwh/Dropbox/Diamond_collaboration'
+dat = pd.read_csv('/Users/jenniferwh/Dropbox (Allen Institute)/Diamond_collaboration/data_files/thresholded_data_LR.csv')
+
+meta = dat[['SampleID', 'Condition']]
+Ldat = dat[[acronym for acronym in dat.columns if '-L' in acronym]]
+Ldat = pd.concat([meta, Ldat])
+Rdat = dat[[acronym for acronym in dat.columns if '-R' in acronym]]
+Rdat = pd.concat([meta, Rdat])
+
+Ldat = pd.melt(Ldat, id_vars = ['SampleID', 'Condition'], value_name = 'density', var_name='acronym')
+Ldat['acronym'] = [acronym[:-2] for acronym in Ldat['acronym'].values]
+Rdat = pd.melt(Rdat, id_vars = ['SampleID', 'Condition'], value_name = 'density', var_name='acronym')
+Rdat['acronym'] = [acronym[:-2] for acronym in Rdat['acronym'].values]
+for structure in Ldat['acronym'].unique():
+    Ldat.loc[Ldat['acronym'] == structure, 'structure_id'] = ia_map[structure]
+    Rdat.loc[Rdat['acronym'] == structure, 'structure_id'] = ia_map[structure]
+
+def get_mean_value_per_structure(group, structure_ids, hemisphere):
     means = []
     structs = []
-    isids = dat[(dat['SampleID'] == group)]['SampleID'].values
+    if hemisphere == 'L':
+        df = Ldat
+    elif hemisphere == 'R':
+        df = Rdat
+    isids = df[(df['SampleID'] == group)]['SampleID'].values
+    print(isids)
     for structure_id in structure_ids:
-        if len(dat[(dat['structure_id'] == structure_id) & 
-                       (dat['SampleID'].isin(isids))]) > 0:
-            str_mean = dat[(dat['structure_id'] == structure_id) & 
-                           (dat['SampleID'].isin(isids))]['density'].mean()
+        if len(df[(df['structure_id'] == structure_id) & 
+                       (df['SampleID'].isin(isids))]) > 0:
+            str_mean = df[(df['structure_id'] == structure_id) & 
+                           (df['SampleID'].isin(isids))]['density'].mean()
             means.append(str_mean+1)
             structs.append(int(structure_id))
     
-    # Not all structures are represented in this dataset. 
+    # Not all structures are represented in this Ldataset. 
     # Color missing structures by their parent
     structures = pd.DataFrame(st.get_structures_by_set_id([184527634]))
     missing_strs = [structure for structure in structures['id'].values if 
@@ -63,9 +78,9 @@ def get_mean_value_per_structure(group, structure_ids):
         ancestors = structures[structures['id'] == structure]['structure_id_path'].values[0]
         for n in range(2, len(ancestors)+1):
             ancestor = ancestors[-n:][0]
-            if ancestor in dat['structure_id'].unique():
-                str_mean = dat[(dat['structure_id'] == ancestor) & 
-                           (dat['SampleID'].isin(isids))]['density'].mean()
+            if ancestor in df['structure_id'].unique():
+                str_mean = df[(df['structure_id'] == ancestor) & 
+                           (df['SampleID'].isin(isids))]['density'].mean()
                 means.append(str_mean)
                 structs.append(structure)
                 missing_strs.remove(structure)
@@ -80,8 +95,8 @@ def get_mean_value_per_structure(group, structure_ids):
     layer_ids += [1121, 526, 20, 52, 543, 664, 92, 712, 139, 727, 28, 60, 743] #ENTl, ENTm
     for structure in layer_ids:
         parent = st.parent([structure])[0]['id']
-        str_mean = dat[(dat['structure_id'] == parent) & 
-                           (dat['SampleID'].isin(isids))]['density'].mean()
+        str_mean = df[(df['structure_id'] == parent) & 
+                           (df['SampleID'].isin(isids))]['density'].mean()
         structuredat[structure] = str_mean
         structuredat[997] = 0
         structuredat[129] = 0
@@ -91,8 +106,8 @@ def get_mean_value_per_structure(group, structure_ids):
         structuredat[145] = 0
     return structuredat, len(isids)
 
-def get_cmap(group, scale=1):
-    structure_vals, n = get_mean_value_per_structure(group, dat['structure_id'].unique())
+def get_cmap(group, hemisphere, scale=1):
+    structure_vals, n = get_mean_value_per_structure(group, Ldat['structure_id'].unique(), hemisphere)
     vals = np.array([value for key, value in structure_vals.items()])
     maxi = np.quantile(vals, 0.95)
     scaled_vals = [value/maxi for key, value in structure_vals.items()]
@@ -104,13 +119,17 @@ def get_cmap(group, scale=1):
 
 def main(params):
     reference_space =  mcc.get_reference_space()
-    structure_vals, n = get_mean_value_per_structure(params['SampleID'], 
-                                                     dat.structure_id.unique())
-    rgb_vals, n = get_cmap(params['SampleID'], scale = 5e-5)
+    structure_vals_L, n = get_mean_value_per_structure(params['SampleID'], 
+                                                     Ldat.structure_id.unique(), 'L')
+    structure_vals_R, n = get_mean_value_per_structure(params['SampleID'], 
+                                                     Rdat.structure_id.unique(), 'R')
+    rgb_vals_L, n = get_cmap(params['SampleID'], 'L')
+    rgb_vals_R, n = get_cmap(params['SampleID'], 'R')
     image = [0,0,0]
-    image[0] = reference_space.get_slice_image(0, params['xcoord'], rgb_vals) #posterior
-    image[1] = np.flip(np.rot90(reference_space.get_slice_image(2, params['zcoord'], rgb_vals)), 0) #right
-    image[2] = np.rot90(reference_space.get_slice_image(1, params['ycoord'], rgb_vals))   #inferior
+    image[0] = reference_space.get_slice_image(0, params['xcoord'], rgb_vals_L) #posterior
+    print(image[0].shape)
+    image[1] = np.flip(np.rot90(reference_space.get_slice_image(2, params['zcoord'], rgb_vals_L)), 0) #right
+    image[2] = np.rot90(reference_space.get_slice_image(1, params['ycoord'], rgb_vals_L))   #inferior
     fig = plt.figure(figsize=(8, 3), facecolor='w')
     columns = 3
     rows = 1
@@ -122,8 +141,8 @@ def main(params):
     cbar.set_label('pTau Probability (per mm$\mathregular{^{3}}$)', 
                    rotation=90, fontsize = 10)
     plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), fontsize = 10)
-    
-    maxval = max(structure_vals.items(), key=operator.itemgetter(1))[1]
+    print(structure_vals_L)
+    maxval = max(structure_vals_L.items(), key=operator.itemgetter(1))[1]
     print(maxval)
     cbar_vals = [0, 
                              np.round(maxval*.2, -3), 
@@ -148,7 +167,7 @@ def main(params):
     
 if __name__ == '__main__':
 
-    with open('params/image_params_single_brain.json', 'r') as data_file:    
+    with open(os.path.join('params', 'image_params_single_brain.json'), 'r') as data_file:    
         parameters = json.load(data_file)
     parameters['SampleID'] = 297
 
